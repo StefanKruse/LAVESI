75393b97e72aa621163e3d0de65b334cbc395a83
Stefan Kruse <stefan.kruse@awi.de>
2017-05-31 10:45:44 +0200

    added Thumbs.db to ignore list changed timer in mortality to real-time not cpu-clocks and modified the output multicore processing of seed mortality seems to be the less costy process - so better search for all processes and check which one uses most of the computation time

---
+++ postprocessing/computationtime_realsecondsOMP.r
@@ -0,0 +1,23 @@
+
+setwd("M:/Documents/Programmierung/git/LAVESI")
+
+t_N_poll=read.table("t_N_poll.txt", sep=";")
+names(t_N_poll)=c("t", "N_seed", "N_tree", "pollination", "treemortality", "seedmortality")
+
+# with(t_N_poll[as.numeric(row.names(t_N_poll))<=10,], plot(pollination~t, type="b", ylim=c(0,with(t_N_poll,max(c(pollination,treemortality,seedmortality)))), xlim=c(0,10)))
+with(t_N_poll[as.numeric(row.names(t_N_poll))<=10,], plot(pollination~t, type="b", ylim=c(0,200), xlim=c(0,10), main="500x500 sqm area"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))<=10,], points(treemortality~t, type="b", col="red"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))<=10,], points(seedmortality~t, type="b", col="orange"))
+
+with(t_N_poll[as.numeric(row.names(t_N_poll))>10 & as.numeric(row.names(t_N_poll))<=19,], points(pollination~t, type="b", col="blue"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))>10 & as.numeric(row.names(t_N_poll))<=19,], points(treemortality~t, type="b", col="red"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))>10 & as.numeric(row.names(t_N_poll))<=19,], points(seedmortality~t, type="b", col="orange"))
+
+
+with(t_N_poll[as.numeric(row.names(t_N_poll))>19,], points(pollination~t, type="b", col="green"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))>19,], points(treemortality~t, type="b", col="red"))
+with(t_N_poll[as.numeric(row.names(t_N_poll))>19,], points(seedmortality~t, type="b", col="orange"))
+
+legend("topleft", pch=1, col=c("black", "blue", "green"), c("Base","8 cores", "8 cores + pollination"), title="mortality.cpp: Pollination and Seed initialization:")
+
+barplot(matrix(c(as.vector(apply(t_N_poll[1:10,4:6],2,mean)/sum(apply(t_N_poll[1:10,4:6],2,mean))),as.vector(apply(t_N_poll[11:19,4:6],2,mean)/sum(apply(t_N_poll[11:19,4:6],2,mean))),as.vector(apply(t_N_poll[20:29,4:6],2,mean)/sum(apply(t_N_poll[20:29,4:6],2,mean)))),byrow=FALSE,ncol=3, nrow=3), names.arg=c("Base", "8 cores", "8 cores + pollination"), legend.text=c("polli","treemort","seedmort"))

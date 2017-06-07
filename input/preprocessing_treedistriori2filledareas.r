a93b97e9198a60a63788d722ba38acec26921c72
Stefan Kruse <stefan.kruse@awi.de>
2017-06-07 15:26:19 +0200

    created the input for 100 m wide and 50000 m long tree covered area

--- input/preprocessing_treedistriori2filledareas.r
+++ input/preprocessing_treedistriori2filledareas.r
@@ -3,9 +3,12 @@
 setwd("M:/Documents/Programmierung/git/LAVESI/input")
 tredat=read.table("CH17I_Treevert2011.csv", sep=";", dec=".", header=TRUE)
 str(tredat)
+# data is already of all trees>100 years and stretched over 100x100 m
 
-write.table(tredat[,2:8], "CH17I_Treevert2011_100_100.csv", sep=";", dec=".", quote=FALSE)
+# write.table(tredat[,2:8], "CH17I_Treevert2011_100_100.csv", sep=";", dec=".", quote=FALSE)
 
+
+{############## <-<- QUADRATIC
 # for(expan in c(5,10))#,20,50))# for each 100*
 for(expan in c(20))#,20,50))# for each 100*
 {
@@ -32,9 +35,35 @@ write.table(trnew, paste0("CH17I_Treevert2011_",100*expan,"_",100*expan,".csv"),
 # with(tredat, plot(jitter(Y,100)~jitter(X,100)))
 # with(tredat, plot(Y~X, cex=1+Hoehe/max(tredat$Hoehe)))
 # with(tredat, plot(Y~X, cex=1+Hoehe/max(tredat$Alter)))
+}
 
 
-# data is already of all trees>100 years and stretched over 100x100 m
+
+
+
+{############## <-<- RECTANGLES==TRANSECTS
+x_expan=1 ## in hundreds
+y_expan=500 ## in hundreds
+
+trnew=NULL
+# expan=5
+for(xei in seq(from=0, to=(x_expan-1), by=1))
+{
+for(yei in seq(from=0, to=(y_expan-1), by=1))
+{
+	print(paste0(xei,"/",yei))
+	id=tredat[,2:8]
+	id$Y=id$Y+100*yei
+	id$X=id$X+100*xei
+	trnew=rbind(trnew, id)
+}
+}
+str(trnew)
+with(trnew, plot(Y~X, cex=1+Hoehe/max(trnew$Hoehe)))
+
+write.table(trnew, paste0("CH17I_Treevert2011_",100*x_expan,"_",100*y_expan,".csv"), sep=";", dec=".", quote=FALSE)
+
+}
 
 
 

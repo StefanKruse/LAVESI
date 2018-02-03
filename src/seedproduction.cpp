8b5b4f4b6dd99eb78015f00fad8e183523f4d102
Stefan Kruse <stefan.kruse@awi.de>
2018-02-03 15:16:02 +0100

    finished work on seedproduction.cpp

--- src/seedproduction.cpp
+++ src/seedproduction.cpp
@@ -1,25 +1,22 @@
 using namespace std;
 
-
 /****************************************************************************************//**
 * \brief calculate  seed production
 *
 * depends on seed production factor, tree size, actual growth
 *******************************************************************************************/
+
 void Seedproduction( struct Parameter *parameter, vector<list<Tree*> > &world_tree_list)
 {
-
-	//int aktort=0;
-	
 	for (vector<list<Tree*> >::iterator posw = world_tree_list.begin(); posw != world_tree_list.end(); ++posw)
-	{ //begin plot list iteration
+	{
 		list<Tree*>& tree_list = *posw;
 
 		for (list<Tree*>::iterator pos = tree_list.begin(); pos != tree_list.end(); )
-		{ //begin tree_list iteration
+		{
 			pTree=(*pos);
 
-			if (pTree->cone==1) // If a tree bears cones seeds will be produced.
+			if (pTree->cone==1)
 			{
 				pTree->seednewly_produced=(int) floor( parameter[0].seedprodfactor	// seed production in dependence of a factor
 												* pTree->height/100					// ... the tree's  height...
@@ -27,12 +24,11 @@ void Seedproduction( struct Parameter *parameter, vector<list<Tree*> > &world_tr
 												* (1.0-(1.0/(pTree->height/50))) );	// ... height.
 				++pos;
 			} 
-			
 			else
 			{
 				++pos;
 			}
-		} //end tree_list iteration
-	}//end plot list iteration
-
+		}
+	}
 }
+

60cfb1a83017a2cd61d7a1063e33fad0bdc4a952
Stefan Kruse <stefan.kruse@awi.de>
2021-02-08 22:33:18 +0100

    set parameters to regional simulation around Lake Ilirney

--- inc/declarations.h
+++ inc/declarations.h
@@ -11,10 +11,10 @@
 // constexpr unsigned int treecols = 7860;
 // constexpr unsigned int treerows = 14310;  // 30m_Ilirney_x641989-649489m_y7476026-7490336m
 // constexpr unsigned int treecols = 7500;
-// constexpr unsigned int treerows = 24720; //30m_Ilirney_x637008.2-655008.2m_y7469996-7494716m
-// constexpr unsigned int treecols = 18000;
-constexpr unsigned int treerows = 14250; //30m_Ilirney_x637008.2-655008.2m_y7469996-7494716m
-constexpr unsigned int treecols = 11430;
+constexpr unsigned int treerows = 24720; //30m_Ilirney_x637008.2-655008.2m_y7469996-7494716m
+constexpr unsigned int treecols = 18000;
+// constexpr unsigned int treerows = 14250; //30m_Ilirney_x637008.2-655008.2m_y7469996-7494716m
+// constexpr unsigned int treecols = 11430;
 
 // parameter input
 constexpr unsigned int stringlengthmax = 255;
